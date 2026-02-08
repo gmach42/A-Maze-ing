641bc49a966020365163e80536610d44a175e79a
Bruno <bruno.fitte@sfr.fr>
2026-02-08 13:10:09 +0100

    Add the recursive division algorithm and it works. Add errors file and rename enum.py because it raise an error due to the fact it has the same name as the standard library

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,3 @@
-from .maze_generator import MazeGenerator
-
 __authors__ = "Gildas", "Bruno"
 __version__ = "0.0.1"
-__all__ = ["MazeGenerator"]
+__all__ = []
--- src/definitions.py
+++ src/definitions.py
@@ -4,3 +4,10 @@ from enum import Enum
 class Orientation(Enum):
     HORIZONTAL = "Horizontal"
     VERTICAL = "Vertical"
+
+
+# class CardinalPoints(Enum):
+#     NORTH = 1
+#     EAST = 2
+#     SOUTH = 4
+#     WEST = 8
