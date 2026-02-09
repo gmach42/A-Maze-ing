9dba55efc0ac7415aea046dd312ec647e68f4636
Bruno <bruno.fitte@sfr.fr>
2026-02-09 14:56:57 +0100

    Try to understand why

--- test_mlx.py
+++ test_mlx.py
@@ -1,4 +1,5 @@
 import sys
+from src.maze_generator import MazeGenerator
 from mlx import Mlx  # Import Mlx class
 # from .solver import Border
 
@@ -70,15 +71,15 @@ def draw_maze_walls(mlx, mlx_ptr, win, maze, cell_size=50):
                 draw_line(mlx, mlx_ptr, win, x, y, x + cell_size, y,
                           0xFFFFFFFF)
 
-            if cell_value & 2:  # SOUTH wall
+            if cell_value & 4:  # SOUTH wall
                 draw_line(mlx, mlx_ptr, win, x, y + cell_size,
                           x + cell_size, y + cell_size, 0xFFFFFFFF)
 
-            if cell_value & 4:  # WEST wall
+            if cell_value & 8:  # WEST wall
                 draw_line(mlx, mlx_ptr, win, x, y, x, y + cell_size,
                           0xFFFFFFFF)
 
-            if cell_value & 8:  # EAST wall
+            if cell_value & 2:  # EAST wall
                 draw_line(mlx, mlx_ptr, win, x + cell_size, y,
                           x + cell_size, y + cell_size, 0xFFFFFFFF)
 
@@ -124,7 +125,9 @@ def main():
 
     # for i in range(xvar.img_1.sl * 100):
     #     xvar.img_1.data[i] = 0xFF
-    test_maze = [[7, 1, 11, 13], [5, 8, 5, 10], [14, 6, 8, 13], [7, 3, 2, 10]]
+    generator: MazeGenerator = MazeGenerator(5, 8)
+    test_maze = generator.get_maze()
+    print(test_maze)
     # draw_line(xvar.mlx, xvar.mlx_ptr, xvar.win_1, (0, 0), (500, 500), 0xFFFFFFFF)
     draw_maze_walls(xvar.mlx, xvar.mlx_ptr, xvar.win_1, test_maze)
 
