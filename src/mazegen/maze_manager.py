59e12e4c57a89d2aa0b6ea71a806fa42721b67c0
gmach <gmach@student.42lyon.fr>
2026-02-16 12:38:02 +0100

    docstring mazemanager

--- src/mazegen/maze_manager.py
+++ src/mazegen/maze_manager.py
@@ -4,9 +4,18 @@ from .solver_algorithm import Solver
 
 
 class MazeManager:
+    """
+    MazeManager class that handles the creation, solving, and output of mazes.
+
+    Attributes:
+        forty_two (list[tuple[int, int]]): List of coordinates representing 42
+            obstacles in the maze, which is used by the solver to avoid these
+            cells when finding the solution path.
+    """
 
     def __init__(self) -> None:
-        self.forty_two: list[tuple[int, int]]
+        """Initialize the MazeManager with an empty list for 42 obstacle"""
+        self.forty_two: list[tuple[int, int]] = []
 
     def get_maze(
         self,
@@ -16,6 +25,7 @@ class MazeManager:
         seed: str | None = None,
         algo: int | None = 1,
     ) -> list[list[int]]:
+        """Generate a maze using the MazeGenerator and store the 42 obstacle"""
         generator: MazeGenerator = MazeGenerator(height, width, perfect, seed,
                                                  algo)
         self.forty_two = generator.forty_two_gps
@@ -23,6 +33,7 @@ class MazeManager:
 
     def solve_maze(self, maze: list[list[int]], start: tuple[int, int],
                    end: tuple[int, int]) -> list[tuple[int, int]]:
+        """Solve the maze using the Solver and return the solution path"""
         solver: Solver = Solver(maze, start, end, self.forty_two)
         return solver.a_star_algorithm()
 
@@ -30,6 +41,7 @@ class MazeManager:
     def create_output_file(maze_matrix: list[list[int]],
                            sol_matrix: list[tuple[int, int]],
                            file_name: str) -> None:
+        """Create an output file for the maze and its solution"""
         output_maze(maze_matrix, sol_matrix, file_name)
 
     def create_complete_maze(
@@ -43,6 +55,7 @@ class MazeManager:
         seed: str | None = None,
         algo: int | None = 1,
     ):
+        """Create a complete maze, solve it, and generate an output file"""
         maze: list[list[int]] = self.get_maze(height, width, perfect, seed,
                                               algo)
         solve: list[tuple[int, int]] = self.solve_maze(maze, start, end)
