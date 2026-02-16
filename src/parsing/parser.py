845618365a7133ed8fd9947936acf9e812e6b436
gmach <gmach@student.42lyon.fr>
2026-02-16 11:19:27 +0100

    rechange x y

--- src/parsing/parser.py
+++ src/parsing/parser.py
@@ -63,7 +63,7 @@ class EnvVariables(BaseModel):
         try:
             val_1: int = int(values[0])
             val_2: int = int(values[1])
-            return (val_1, val_2)
+            return (val_2, val_1)
         except ValueError:
             raise ValueError("Entry and exit must be integers!")
 
